eab91ccdb55efe3a9871134a0a5421ecb11aa672
rain9155 <jianyu9155@gmail.com>
2018-10-21 10:37:23 +0800

    first commit

---
+++ src/com/example/hy/01.基本数据类型.kt
@@ -0,0 +1,18 @@
+package com.example.hy
+
+/**
+ * 8种基本数据类型
+ */
+fun main(args: Array<String>) {
+    var short : Short = 1//2byte
+    var int : Int = 2//4byte
+    var long  : Long = 3//8byte
+    var float : Float = 4f//4byte
+    var double : Double = 6.0//8byte
+    var char : Char = 'n'//2byte
+    var b : Boolean = false//1byte
+    var byte : Byte = 10//1byte
+
+    println(int.hashCode())
+
+}
