f2d299d6f95ad071b1c5f3e260cfc52f2302fe66
rain9155 <jianyu9155@gmail.com>
2018-11-02 11:21:47 +0800

    添加了数组查找修改的笔记

--- src/com/example/hy/kotlin_day_02/07.数组的创建.kt
+++ src/com/example/hy/kotlin_day_02/07.数组的创建.kt
@@ -2,7 +2,7 @@ package com.example.hy.kotlin_day_02
 
 /**
  * kotlin数组的创建：
- * 8种基本数据类型都能通过arrayOf创建或自己类型的Array创建
+ * 8种基本数据类型的数组都能通过arrayOf创建或自己类型的Array创建
  * ps：String例外，只能通过arrayOf创建，不能通过自己类型的Array创建
  * ps: Any相当于java中的Object对象（可以代表任何类型）
  */
@@ -18,7 +18,7 @@ fun main(args: Array<String>) {
     val array6 = arrayOf('a', 10, "李四") //本质上这个数组每个元素都为Any类型 ps：Any相当于java中的Object对象（可以代表任何类型）
     //其他类型以此类推...
 
-    /** 通过自己类型的Array创建但不赋值 */
+    /** 通过自己类型的Array创建数组但不赋值 */
     //需求1：定义一个容量为3的整形数组
     val array4 = IntArray(3)
     //需求2：定义一个容量为3的整形数组，所有数据都初始化为3
