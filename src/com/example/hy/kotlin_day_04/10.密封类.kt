86fa8c1a1df99ec1df5368420da6fe40201a60f4
rain9155 <jianyu9155@gmail.com>
2018-12-21 15:38:37 +0800

    添加了密封类和数据类

---
+++ src/com/example/hy/kotlin_day_04/10.密封类.kt
@@ -0,0 +1,58 @@
+package com.example.hy.kotlin_day_04
+
+import com.example.hy.kotlin_day_02.islegal
+
+/**
+ * kotlin的密封类
+ * 1、特点：
+ * 超强的枚举
+ * 2、与枚举的区别：
+ * 密封类封装的是类型（类型确定），而枚举封装的是数据（数据确定）
+ * 3、定义：
+ * class前加sealed关键字
+ */
+fun main(args: Array<String>) {
+
+    //需求：权力的游戏，斯塔克选继承人
+    val jonSnow = JonSnow()
+    println(hasRight(jonSnow))//私生子没有权利，输出：false
+
+    val aryaStark = NedStark.AryaStark()
+    println(hasRight(aryaStark))//亲生的，有权利，输出：true
+
+}
+
+/**
+ * 判断是否有权力继承王位
+ */
+fun hasRight(stark : NedStark) : Boolean{
+    return when(stark){
+        is NedStark.AryaStark -> true
+        is NedStark.BrandonStark -> true
+        is NedStark.RobStark -> true
+        is NedStark.SansaStark -> true
+        else -> false
+    }
+}
+
+/**
+ * 斯塔克
+ */
+sealed class NedStark{
+
+    /** 斯塔克的亲生子女 */
+
+    class RobStark : NedStark()
+
+    class SansaStark : NedStark()
+
+    class AryaStark : NedStark()
+
+    class BrandonStark : NedStark()
+
+}
+
+/**
+ * 斯塔克的私生子
+ */
+class JonSnow : NedStark()
---
+++ src/com/example/hy/kotlin_day_04/11.数据类.kt
@@ -0,0 +1,47 @@
+package com.example.hy.kotlin_day_04
+
+/**
+ * kotlin的数据类
+ * 1、特点：
+ * 只保存数据，没有其他任何逻辑操作，对应java的bean类
+ * 主要解决java中定义bean类时产生大量重复代码
+ * 2、定义：
+ * class前加data关键字
+ */
+fun main(args: Array<String>) {
+
+    //用法1：
+    val news = News("rain", "内容", "2018/12/21")
+    println(news.title)
+    println(news.content)
+    println(news.time)
+
+    //用法2：
+    println(news.component1())
+    println(news.component2())
+    println(news.component3())
+
+    //用法3:
+    val (title, content, time) = News("rain", "内容", "2018/12/21")
+    println(title)
+    println(content)
+    println(time)
+
+    //以上全部输出：
+    //rain
+    //内容
+    //2018/12/21
+
+}
+
+/**
+ * 加data相当于在类中自动生成了：
+ * 1、构造函数，并在构造函数里给字段赋值
+ * 2、字段的get，set方法
+ * 3、字段对应的component方法
+ * 4、类的copy（）方法
+ * 5、toString（）方法
+ * 6、hashCode()方法
+ * 7、equals()方法
+ */
+data class News(var title : String, var content : String, var time : String)
