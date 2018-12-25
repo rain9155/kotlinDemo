b411b49e2e37c7344b9ec6e66ff395011fc508a2
rain9155 <jianyu9155@gmail.com>
2018-12-25 12:59:23 +0800

    添加闭包

---
+++ src/com/example/hy/kotlin_day_05/04.闭包.kt
@@ -0,0 +1,41 @@
+package com.example.hy.kotlin_day_05
+
+/**
+ * kotlin的闭包（函数式编程）
+ * 1、定义：
+ * 相当于lambda表达式，函数既可以作为方法返回值，又可以作为函数参数
+ */
+fun main(args: Array<String>) {
+
+    //调用三次test（），a输出都是0
+    test()// 0
+    test()// 0
+    test()// 0
+
+    //使用闭包，调用三次test2（），每次输出值不一样
+    val result = test2()
+    result.invoke()// 0
+    result()// 1
+    result()// 2
+
+}
+
+/**
+ * 传统函数不会保存变量状态
+ */
+fun test(){
+    var a = 0
+    println(a)
+    a++
+}
+
+/**
+ * 闭包，返回一个函数
+ */
+fun test2() : () -> Unit{
+    var a = 0
+    return {
+        println(a)
+        a++
+    }
+}
