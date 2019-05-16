eb309e89e0575e56583f9c1188332f49db1883d1
rain9155 <jianyu9155@gmail.com>
2019-05-16 19:25:39 +0800

    first commit

--- src/com/example/hy/kotlin_day_01/01.基本数据类型.kt
+++ src/com/example/hy/kotlin_day_01/01.基本数据类型.kt
@@ -4,6 +4,7 @@ package com.example.hy.kotlin_day_01
  * 8种基本数据类型
  */
 fun main(args: Array<String>) {
+
     var short : Short = 1//2byte
     var int : Int = 2//4byte
     var long  : Long = 3//8byte
